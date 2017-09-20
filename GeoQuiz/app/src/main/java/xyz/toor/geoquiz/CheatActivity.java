41d4b5fb47df64e2198f11afa8cca7d0add0f7d2
chalypeng <chalypeng@gmail.com>
2017-09-20 17:33:36 +0800

    Done Chap 6.

--- GeoQuiz/app/src/main/java/xyz/toor/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/xyz/toor/geoquiz/CheatActivity.java
@@ -1,35 +1,40 @@
 package xyz.toor.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
     private static final String TAG = "CheatActivity";
-    private static final String EXTRA_ANSWER_IS_TRUE="xyz.toor.geoquiz.answer_is_true";
-    private static final String EXTRA_ANSWER_SHOWN="xyz.toor.geoquiz.answer_shown";
+    private static final String EXTRA_ANSWER_IS_TRUE = "xyz.toor.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "xyz.toor.geoquiz.answer_shown";
 
-    private static final String KEY_ISCHEATED="isCheat";
-    private static final String KEY_ANSWER_TEXT ="answer_text";
+    private static final String KEY_ISCHEATED = "isCheat";
+    private static final String KEY_ANSWER_TEXT = "answer_text";
 
     private boolean mAnswerIsTrue;
     private Button mShowAnswerButton;
     private TextView mAnswerTextView;
+    private TextView mAPILevelTextView;
 
     private boolean mIsCheater;
 
-    public static Intent newIntent(Context packageContext,boolean answerIsTrue){
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
-        intent.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return intent;
     }
 
-    public static boolean wasAnswerShown(Intent result){
-        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 
     @Override
@@ -37,43 +42,64 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
-        if (savedInstanceState !=null){
-            mIsCheater = savedInstanceState.getBoolean(KEY_ISCHEATED,false);
-            if(mIsCheater){
+        if (savedInstanceState != null) {
+            mIsCheater = savedInstanceState.getBoolean(KEY_ISCHEATED, false);
+            if (mIsCheater) {
                 setAnswerShownResult(true);
                 mAnswerTextView.setText(savedInstanceState.getString(KEY_ANSWER_TEXT));
             }
         }
 
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mAnswerIsTrue){
+                if (mAnswerIsTrue) {
                     mAnswerTextView.setText(R.string.true_button);
-                }else {
+                } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 mIsCheater = true;
                 setAnswerShownResult(true);
+
+                // LOLLIPOP 时使用动画
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = null;
+                    anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
-
-
         });
+
+        mAPILevelTextView = (TextView) findViewById(R.id.api_level_text_view);
+        mAPILevelTextView.setText("API Level " + Build.VERSION.SDK_INT);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putBoolean(KEY_ISCHEATED,mIsCheater);
-        savedInstanceState.putString(KEY_ANSWER_TEXT,mAnswerTextView.getText().toString());
+        savedInstanceState.putBoolean(KEY_ISCHEATED, mIsCheater);
+        savedInstanceState.putString(KEY_ANSWER_TEXT, mAnswerTextView.getText().toString());
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
-        setResult(RESULT_OK,data);
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
     }
 }
