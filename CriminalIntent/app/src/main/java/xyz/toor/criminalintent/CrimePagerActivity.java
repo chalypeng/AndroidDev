acbbee8f9282408ee9f58bf3fa4be99bd9278e13
chalypeng <chalypeng@gmail.com>
2017-09-22 13:52:00 +0800

    done Challenge 11

--- CriminalIntent/app/src/main/java/xyz/toor/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/xyz/toor/criminalintent/CrimePagerActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,10 +24,12 @@ public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private Button mJumpToFirstButton;
+    private Button mJumpToLastButton;
 
-    public static Intent newIntent(Context packageContext, UUID crimeId){
-        Intent intent = new Intent(packageContext,CrimePagerActivity.class);
-        intent.putExtra(EXTRA_CRIME_ID,crimeId);
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
         return intent;
     }
 
@@ -40,13 +43,15 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
 
+
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
                 Crime crime = mCrimes.get(position);
-                return CrimeFragement.newInstance(crime.getId());
+                CrimeFragement crimeFragement = CrimeFragement.newInstance(crime.getId());
+                return crimeFragement;
             }
 
             @Override
@@ -54,11 +59,58 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
-        for (int i=0;i<mCrimes.size();i++){
-            if (mCrimes.get(i).getId().equals(crimeId)){
-                mViewPager.setCurrentItem(i,true);
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i, true);
                 break;
             }
         }
+
+        mJumpToFirstButton = (Button) findViewById(R.id.jump_to_first_button);
+        mJumpToFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0, true);
+            }
+        });
+        mJumpToLastButton = (Button) findViewById(R.id.jump_to_last_button);
+        mJumpToLastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size()-1);
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (mViewPager.getCurrentItem()==0){
+                    mJumpToFirstButton.setEnabled(false);
+                }else{
+                    mJumpToFirstButton.setEnabled(true);
+                }
+//                if (position==0){
+//                    mJumpToFirstButton.setEnabled(false);
+//                }else{
+//                    mJumpToFirstButton.setEnabled(true);
+//                }
+                if (mViewPager.getAdapter().getCount()-1==mViewPager.getCurrentItem()){
+                    mJumpToLastButton.setEnabled(false);
+                }else{
+                    mJumpToLastButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
     }
 }
